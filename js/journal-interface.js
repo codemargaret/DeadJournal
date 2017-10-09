315959f878fbe64e3d31f1f995c826d3af7b1b6c
Margaret Berry and Sara Spink <sara@>
2017-10-09 13:54:20 -0700

    allows a user to enter a title and a journal entry

--- js/journal-interface.js
+++ js/journal-interface.js
@@ -1,14 +1,20 @@
 var DJEntry = require('./../js/journal.js').DJEntryModule;
 
 $(document).ready(function() {
+  console.log("document ready");
   $('#entry-form').submit(function(event) {
+
     event.preventDefault();
+    console.log("entry form submit");
     var title = $('#title').val();
     var body = $('#body').val();
+    console.log(body);
     var newEntry = new DJEntry();
-    var output;
-    $("#titleoutput").text(output);
 
-    });
+    // var output = $()
+
+    $("#titleoutput").append("<li><h1>" + title + "</h1></li>");
+    $("#titleoutput").append("<li><h3>" + body + "</h3></li>");
+
   });
 });
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,15 +1,20 @@
 var DJEntry = require('./../js/journal.js').DJEntryModule;
 
 $(document).ready(function() {
-  $('#title-form').submit(function(event) {
+  console.log("document ready");
+  $('#entry-form').submit(function(event) {
+
     event.preventDefault();
+    console.log("entry form submit");
     var title = $('#title').val();
     var body = $('#body').val();
+    console.log(body);
     var newEntry = new DJEntry();
-    var output;
 
-    $("#entry-submit").click(function() {
-      console.log("test phrase");
-    });
+    // var output = $()
+
+    $("#titleoutput").append("<li><h1>" + title + "</h1></li>");
+    $("#titleoutput").append("<li><h3>" + body + "</h3></li>");
+
   });
 });
